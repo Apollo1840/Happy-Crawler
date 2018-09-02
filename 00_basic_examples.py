fee13643cc6878a382103d2d211cf9ef1bb59d5c
Apollo1840 <zoucongyu1993@hotmail.com>
2018-09-03 00:51:25 +0200

    another crawler

--- 00_basic_examples.py
+++ 00_basic_examples.py
@@ -1,11 +1,52 @@
 # -*- coding: utf-8 -*-
 
+'''
+basically there are two ways to create crawler:
+    1) urllib.request :  from urllib import request
+        .urlretrieve
+        .urlopen
+            str(file.read())
+            json.loads(file.read())
+    
+    2) requests
+        response = requests.get()
+        
+        response.content
+        response.text
+
+1. host
+
+    1) request
+    before using the request:
+            
+        opener = request.build_opener()
+            opener.addheaders = [
+            ('User-Agent',
+             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        request.install_opener(opener)
+        
+    2) requests
+    when use the request.get()
+        
+        hea = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
+        response = requests.get(url,headers = hea)
+
+2, beatifulsoup
+    the response.text can be insert into beatifulsoup
+        soup = BeautifulSoup(response.text, 'html5lib')
+            
+
+
+
+'''
+
+
+
+
+
 from urllib import request 
 import json
 import os
-
-import requests as req # more useful than urllib
-from bs4 import BeautifulSoup
 
 # use request in urllib to download img and csv
 
@@ -24,7 +65,7 @@ def download_csv(url):
 def download_csv_dumps(url):
     # download a csv_file website and show the content
     file = request.urlopen(url)
-    J = json.loads(file.read())
+    J = json.loads(file.read())  # json.loads can take bit information
     json.dumps(J, indent = 2)
 
 
@@ -46,7 +87,8 @@ def simple_show_case():
 
 
 ###############################################################################
-
+import requests as req # more useful than urllib
+from bs4 import BeautifulSoup
 
 def show_case_by_requests():
     
@@ -70,7 +112,7 @@ def show_case_by_requests():
     response.encoding = 'utf-8'
     with open('material/page.html','w', encoding='utf-8') as f:
         html = response.text
-        f.write(html)   # .text is html format text
+        f.write(html)                # .text is html format text
     
 # use 'get' in requests to get the response(html) of the website
     
--- 02_learn_beatifulsoup.py
+++ 02_learn_beatifulsoup.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 with open('test.html') as f:
     soup = BeautifulSoup(f,'lxml')
+    
+    # xml, lxml, html5lib, html.parser
   
 print(soup.prettify())
 
@@ -14,3 +16,7 @@ print(article.prettify())
 article.h2.a.text
 
 article.find('iframe', class_='youtube-player')['src']  # src is the attribute of tag
+
+
+for article in soup.find_all('div', class_='footer'):
+    print(article.h2.a.text)
