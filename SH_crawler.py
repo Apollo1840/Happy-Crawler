974b3fbffcef3ec32f219d7a10f61c5ddbb2fbb5
Apollo1840 <zoucongyu1993@hotmail.com>
2018-09-24 17:26:18 +0200

    modified

--- SH_crawler.py
+++ SH_crawler.py
@@ -11,7 +11,7 @@ import pandas as pd
 import time
 
 def refine_wiki_link(link):
-    if link.startswith('https://de.wikipedia.org'):
+    if link.startswith('https://de.wikipedia.org') or link.startswith('https://en.wikipedia.org'):
         return link
     else:
         pieces = link.split('=')
@@ -20,13 +20,14 @@ def refine_wiki_link(link):
             link += piece
         pieces = link.split('&')
         link = pieces[0]
-        if link.startswith('https://de.wikipedia.org'):
+        if link.startswith('https://de.wikipedia.org') or link.startswith('https://en.wikipedia.org'):
             return link
         else:
+            print('\n{}\n!!! wrong link !!!\n'.format(link))
             return 'https://www.google.com'
         
 
-def get_umsatz_supplier(supplier_name):
+def get_revenue_supplier(supplier_name):
         supplier = supplier_name.replace(' ','+') + '+wiki'
         print(supplier)
         
@@ -43,86 +44,80 @@ def get_umsatz_supplier(supplier_name):
         link = refine_wiki_link(link)
         print(link)
       
+        
         soup = bc.get_soup(link)
         time.sleep(3)
-        table = soup.select('#Vorlage_Infobox_Unternehmen')
-        revenue = None
-        if table:
-            for row in table[0].find_all('tr'):
-                try:
-                    if str.strip(row.find('td').text) == 'Umsatz':
-                        revenue = row.find_all('td')[1].text
-                        print(supplier_name, revenue)
-                except AttributeError:
-                    pass
         
-        return revenue 
+        revenue = None
+        if link.startswith('https://de.wikipedia.org'):
+            revenue = get_revenue_in_soup_de(soup)
+        elif link.startswith('https://en.wikipedia.org'):
+            revenue = get_revenue_in_soup_en(soup)
+        else:
+            print(link)
+        
+        return revenue
+        
+        
+        
+    
+
+def get_revenue_in_soup_de(soup):
+    table = soup.select('#Vorlage_Infobox_Unternehmen')
+    revenue = None
+    if table:
+        for row in table[0].find_all('tr'):
+            try:
+                if str.strip(row.find('td').text) == 'Umsatz':
+                        revenue = row.find_all('td')[1].text
+                        print(revenue)
+            except AttributeError:
+                pass
+        
+    return revenue 
+
+
+
+def get_revenue_in_soup_en(soup):
+    table = soup.select('#mw-content-text > div > table.infobox.vcard')[0]
+    revenue = None
+    if table:
+        for row in table.find_all('tr'):
+            try:
+                if str.strip(row.find('th').text) == 'Revenue':
+                    revenue = row.find('td').text
+                    print(revenue)
+            except AttributeError:
+                    pass
+    return revenue 
+    
+
+    
     
     
 
 
 if __name__ == '__main__':
     
+    pd.set_option('max_colwidth',200)
+    pd.set_option('max_columns',None) 
+    
     suppliers = ["Jabil", "Sanmina", "Thermo Fisher Diagnostics", "IVEK", "Randox Laboratories", "Inpeco", "Nypro", "Carclo", "Bio-Rad Laboratories"]
     
     revenues = []
     for supplier in suppliers:
-        revenues.append(get_umsatz_supplier(supplier))
+        revenues.append(get_revenue_supplier(supplier))
     
     result = pd.DataFrame()
     result['supplier']=suppliers
     result['revenue'] = revenues
     
+    print(result)
     result.to_csv('SPIDER.csv')
         
         
         
         
         
-    
-    supplier = 'Sanmina+wiki'
-    bc = BasicCrawler(headers='auto')
-    search_link = 'https://www.google.com/search?q={}'.format(supplier)
-    
-    print(search_link)
-    
-    soup = bc.get_soup(search_link)
-    time.sleep(3)
-    a = soup.find_all('h3', class_='r')[0]
-    link = a.find('a')['href']
-    
-    print(link)
-      
-    soup = bc.get_soup(link)
-    time.sleep(3)
-    print(soup)
-    table = soup.select('#Vorlage_Infobox_Unternehmen')
 
-    for row in table[0].find_all('tr'):
-        try:
-            if str.strip(row.find('td').text) == 'Umsatz':
-                print(supplier,row.find_all('td')[1].text)
-        except AttributeError:
-            pass
-    
-                       
-    
-    #mw-content-text > div > table
-
-import requests
-
-def googleSearch(query):
-    with requests.session() as c:
-        url = 'https://www.google.co.in'
-        query = {'q': query}
-        urllink = requests.get(url, params=query)
-        print(urllink.url)
-
-googleSearch('Linkin Park')
-soup = bc.get_soup('https://www.google.com/search?q=Bio-Rad+Park+wiki')
-a = soup.find_all('h3', class_='r')
-
-print(anchors)
-anchors[0].text
-print([t.find('a').text for t in a])
     
