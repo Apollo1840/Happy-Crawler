e43576ac4b37d51cc8f9461f60daa67e9bad2297
Apollo1840 <zoucongyu1993@hotmail.com>
2018-09-25 17:01:39 +0200

    1 day

--- SPIDER_crawler.py
+++ SPIDER_crawler.py
@@ -117,27 +117,59 @@ def get_thomasnet_supplier_links(supplier_names=[]):
 '''
 
 class ThomasnetCrawler():
-
+    
+    def __init__(self):
+        self.df = None
 
     def get_suppliers(self, material='plastics'):
         # the material name must be prefict
         
         url = 'https://www.thomasnet.com/catalogs/keyword/{}/'.format(material)
         bc = BasicCrawler()
-        res = bc.get_soup(url, safetime=(3,5))
+        res = bc.get_soup(url, safetime=(1,1))
         
         companies = res.find_all('div', class_='company')
         company_names = [i.find('div', class_ = 'coname').text for i in companies]
    
         return company_names
     
-
+    def is_suppliers_page(self, soup):
+        title = soup.find('h1', class_='supplier-search-results__page-title').text
+        return title.endswith('Suppliers')
+    
+    def get_suppliers_link_general(self, material = 'plastics'):
+        
+        material = 'plastics'
+        s = material.replace(' ','+')
+        url = 'https://www.thomasnet.com/search.html?WTZO=Find+Suppliers&cov=NA&searchx=true&what={}&which=prod'.format(s)
+        
+        bc = BasicCrawler()
+        soup = bc.get_soup(url)
+        
+        if self.is_suppliers_page(soup):
+            soup2 = soup
+        else:
+            table = soup.find('table', class_='table-striped')
+            rows = table.find_all('tr')
+            link = 'https://www.thomasnet.com/' + rows[0].find('td').find('a')['href']
+            soup2 = bc.get_soup(link)
+        
+        divs = soup2.find_all('div', class_='profile-card--secondary')
+        company_names = []
+        company_links = []
+        for div in divs:
+            anchor = div.find('h2').find('a')
+            company_names.append(anchor.text)
+            company_links.append('https://www.thomasnet.com/' + anchor['href'])
+        
+        print(company_names, company_links)
+        
 
     def get_thomasnet_supplier_link(self, supplier_name = 'Industrial Plastic Supply'):
         search_name = re.sub('[^\w]',' ', supplier_name).replace(' ','+')
         url = "https://www.thomasnet.com/search.html?WTZO=Find+Suppliers&cov=NA&searchx=true&what={}&which=prod".format(search_name)
         bc = BasicCrawler()
-        res = bc.get_soup(url, safetime=(3,5))
+        res = bc.get_soup(url, safetime=(1,1))
         box = res.find('div',class_="search-list")
     
         link = None
@@ -145,16 +177,19 @@ class ThomasnetCrawler():
             anchor = box.find('li').find('a')
             link = 'https://www.thomasnet.com/' + anchor['href']
         
-        print(link)
+        print('Give me 3 seconds, I am scraping the information of {} ...'.format(supplier_name))
         
         return link
     
     def get_bizdetails(self, url_company = 'https://www.thomasnet.com//profile/00281106/industrial-plastic-supply-inc.html?cid=281106&cov=NA&searchpos=1&what=Industrial+Plastic+Supply&which=comp'):
         name = None
         revenue = None
+        num_employee = None
+        year_founded = None
+        
         
         bc = BasicCrawler()
-        res = bc.get_soup(url_company, safetime=(3,5))
+        res = bc.get_soup(url_company, safetime=(1,2))
         
         anchor = res.select('#copro_naft > div.codetail > h1')
         name = anchor[0].text
@@ -167,15 +202,19 @@ class ThomasnetCrawler():
         for bd in bds:
             try:
                 bd_header = str.strip(bd.find('div').text)
+                if bd_header == 'Annual Sales:':
+                    revenue = bd.find('li').text
+                if bd_header == 'No of Employees:':
+                    num_employee = bd.find('li').text
+                if bd_header == 'Year Founded:':
+                    year_founded = bd.find('li').text
+           
             except Exception:
                  bd_header = None
-            
-            if bd_header == 'Annual Sales:':
-                revenue = bd.find('li').text
-        
-        return name, link, revenue
+
+        return name, link, revenue, num_employee, year_founded
     
-    def run(self, materials):
+    def run(self, material, number_suppliers=3):
         # the material name must be prefict
         
         df = pd.DataFrame()
@@ -183,21 +222,37 @@ class ThomasnetCrawler():
         names = []
         links = []
         revenues = []
+        materials = []
+        num_employees = []
+        year_foundeds = []
         
-        for material in materials:
-            supplier_names = self.get_suppliers(material)
-            link = self.get_thomasnet_supplier_link(supplier_names[0])
-            name, link, revenue = self.get_bizdetails(link)
+        
+        supplier_names = self.get_suppliers(material)
+        for supplier in supplier_names[:number_suppliers]:
+            link = self.get_thomasnet_supplier_link(supplier)
+            name, link, revenue, num_employee, year_founded = self.get_bizdetails(link)
             
+            materials.append(material)
             names.append(name)
             links.append(link)
             revenues.append(revenue)
+            num_employees.append(num_employee)
+            year_foundeds.append(year_founded)
+            
         
+        df['material'] = materials
         df['name'] = names
         df['link'] = links
         df['revenue'] = revenues
+        df['num_employee'] = num_employees
+        df['year_founded'] = year_founded
         
+        
+        
+        self.df = df
         print(df)
+        df.to_csv('SPIDER_{}.csv'.format(material))
+        return df
 
     
 def test():
@@ -223,7 +278,9 @@ if __name__ == '__main__':
     pd.set_option('max_columns',None) 
     
     tc = ThomasnetCrawler()
-    tc.run(['plastics'])
+    #df = tc.run('plastics', number_suppliers=3)
+    
+    df = tc.run('chemicals', number_suppliers=4)
     
 
     
